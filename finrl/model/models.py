2d535115470e6e1feeb83f1b82519934802a60cb
Biaolin Wen <robinbg@foxmail.com>
2020-12-12 10:52:28 +0800

    Update models.py

--- finrl/model/models.py
+++ finrl/model/models.py
@@ -43,6 +43,7 @@ class CustomActorCriticPolicy(ActorCriticPolicy):
             **kwargs,
         )
         # Disable orthogonal initialization
+        self.ortho_init = False
 
     @staticmethod
     def init_weights(module: nn.Module, gain: float = 1) -> None:
